40233e7ab10c31cb5c6957fc4c481548fc99eaef
casperwasher <113210251+casperwasher@users.noreply.github.com>
2023-02-13 03:47:52 +0700

    Add files via upload

--- baseikal/command/bugmenu/bugmenu.js
+++ baseikal/command/bugmenu/bugmenu.js
@@ -1,167 +1,152 @@
 const bugmenu =  ` 
 ° Owner  : Casper
-° Version : 13
-° Baileys : 4.4.0
+° Version : 25
+° Baileys : 5.4.0
 
-Created : 😈 ${botname} 👿
-▬▭▬▭▬▭▬▭▬▬▭▬▭▬
-*EMOJI SPAM*
+Created : ❏ ${botname} ❏
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+*BUG NEW*
 ┏━━⊱
-┣❏🦖 [ jumlah ]
-┣❏🦕 [ jumlah ]
-┣❏👿 [ jumlah ]
-┣❏⚡ [ jumlah ]
-┣❏😈 [ jumlah ]
-┣❏💥 [ jumlah ]
-┣❏🍂 [ jumlah ]
-┣❏🌪️ [ jumlah ]
-┣❏🔥 [ Jumlah ]
+┣⊱ Jagoantod [ jumlah ]
+┣⊱ Sokkeras [ jumlah ]
+┣⊱ Endbro [ Jumlah ]
 ┗━━⊱
-[ contoh 👿 5 ]
-▬▭▬▭▬▭▬▭▬▬▭▬▭▬
-*SEND BUG EMOJI*
+[ contoh Endbro 5 ]
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+*SEND BUG NEW*
 ┏━━⊱
-┣❏🌷 628xxx
-┣❏🐲 628xxx
-┣❏🐉 628xxx
-┣❏🌵 628xxx
-┣❏🎄 628xxx
-┣❏🌲 628xxx
-┣❏🌳 628xxx
-┣❏🌴 628xxx
-┣❏🌱 628xxx
-┣❏🌿 628xxx
-┣❏☘️ 628xxx
-┣❏🍀 628xxx
+┣⊱ Killbro 628xxx
+┣⊱ Santetbro️ 628xxx
 ┗━━⊱
-[ contoh 🌷 6281410528015 ]
-▬▭▬▭▬▭▬▭▬▬▭▬▭▬
-*VIRTEXT DELAY*
-┏━━⊱
-┣❏Virtext [ jumlah ]
-┣❏Virtext2 [ jumlah ]
-┣❏Virtext3 [ jumlah ]
-┣❏Virtext4 [ jumlah ]
-┣❏Virtext5 [ jumlah ]
-┣❏Virtext6 [ jumlah ]
-┣❏Virtext7 [ jumlah ]
-┣❏Virtext8 [ jumlah ]
-┣❏Virtext9 [ jumlah ]
-┣❏Virtext10 [ jumlah ]
-┗━━⊱
-[ Contoh Virtext1 5 ]
-▬▭▬▭▬▭▬▭▬▬▭▬▭▬
-*SEND BUG TROLI*
-┏━━⊱
-┣❏Sendtrol 628xxxx
-┣❏Sendtrol2 628xxx
-┣❏Sendtrol3 628xxx
-┣❏Sendtrol4 628xxx
-┣❏Sendtrol5 628xxx
-┗━━⊱
-[ contoh Sendtrol 6281410528015 ]
-▬▭▬▭▬▭▬▭▬▬▭▬▭▬
-*SEND BUG SANTET*
-┏━━⊱
-┣❏Santet 628xxxx
-┣❏Santet2 628xxx
-┣❏Santet3 628xxx
-┣❏Santet4 628xxx
-┣❏Santet5 628xxx
-┗━━⊱
-[ contoh Santet 6281410528015 ]
-▬▭▬▭▬▭▬▭▬▬▭▬▭▬
-*SEND BUG DOKUMEN*
-┏━━⊱
-┣❏Senddocu 628xxxx
-┣❏Senddocu2 628xxx
-┣❏Senddocu3 628xxx
-┣❏Senddocu4 628xxx
-┣❏Senddocu5 628xxx
-┗━━⊱
-[ contoh Senddocu 6281410528015 ]
-▬▭▬▭▬▭▬▭▬▬▭▬▭▬
-*SEND BUG LOKASI*
-┏━━⊱
-┣❏Sendlokas 628xxxx
-┣❏Sendlokas2 628xxx
-┣❏Sendlokas3 628xxx
-┣❏Sendlokas4 628xxx
-┣❏Sendlokas5 628xxx
-┗━━⊱
-[ contoh Sendlokas 6281410528015 ]
-▬▭▬▭▬▭▬▭▬▬▭▬▭▬
-*SEND BUG INVITE*
-┏━━⊱
-┣❏Sendinvite 628xxxx
-┣❏Sendinvite2 628xxx
-┣❏Sendinvite3 628xxx
-┣❏Sendinvite4 628xxx
-┣❏Sendinvite5 628xxx
-┗━━⊱
-[ contoh Sendinvite 6281410528015 ]
-▬▭▬▭▬▭▬▭▬▬▭▬▭▬
-*SEND BUG LIST*
-┏━━⊱
-┣❏Sendbuglist 628xxxx
-┣❏Sendbuglist2 628xxx
-┣❏Sendbuglist3 628xxx
-┣❏Sendbuglist4 628xxx
-┣❏Sendbuglist5 628xxx
-┗━━⊱
-[ contoh Sendbuglist 6281410528015 ]
-▬▭▬▭▬▭▬▭▬▬▭▬▭▬
-*SEND VIRUS IP / ANDRO*
-┏━━⊱
-┣❏Sendvirus 628xxxx
-┣❏Sendvirus2 628xxx
-┣❏Sendvirus3 628xxx
-┣❏Sendvirus4 628xxx
-┣❏Sendvirus5 628xxx
-┣❏Sendvirus6 628xxx
-┣❏Sendvirus7 628xxx
-┣❏Sendvirus8 628xxx
-┣❏Sendvirus9 628xxx
-┣❏Sendvirus10 628xx
-┗━━⊱
-[ contoh Sendvirus 6281410528015 ]
-▬▭▬▭▬▭▬▭▬▬▭▬▭▬
+[ contoh Santetbro 628956666 ]
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 *SEND BUG GROUP*
 ┏━━⊱
-┣❏🌹 12637xxx
+┣⊱ Endgc 12637xxx
 ┗━━⊱
-[ contoh 🌹 12637xxx ]
+[ contoh Endgc 12637xxx ]
+
 *Notes : pakai id Group*
-▬▭▬▭▬▭▬▭▬▬▭▬▭▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+*VIRTEXT DELAY*
+┏━━⊱
+┣⊱ Virtext [ jumlah ]
+┣⊱ Virtext2 [ jumlah ]
+┣⊱ Virtext3 [ jumlah ]
+┣⊱ Virtext4 [ jumlah ]
+┣⊱ Virtext5 [ jumlah ]
+┣⊱ Virtext6 [ jumlah ]
+┣⊱ Virtext7 [ jumlah ]
+┣⊱ Virtext8 [ jumlah ]
+┣⊱ Virtext9 [ jumlah ]
+┣⊱ Virtext10 [ jumlah ]
+┗━━⊱
+[ Contoh Virtext1 5 ]
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+*SEND BUG TROLI*
+┏━━⊱
+┣⊱ Sendtrol 628xxxx
+┣⊱ Sendtrol2 628xxx
+┣⊱ Sendtrol3 628xxx
+┣⊱ Sendtrol4 628xxx
+┣⊱ Sendtrol5 628xxx
+┗━━⊱
+[ contoh Sendtrol 6281410528 ]
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+*SEND BUG SANTET*
+┏━━⊱
+┣⊱ Santet 628xxxx
+┣⊱ Santet2 628xxx
+┣⊱ Santet3 628xxx
+┣⊱ Santet4 628xxx
+┣⊱ Santet5 628xxx
+┗━━⊱
+[ contoh Santet 628141052801 ]
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+*SEND BUG DOKUMEN*
+┏━━⊱
+┣⊱ Senddocu 628xxxx
+┣⊱ Senddocu2 628xxx
+┣⊱ Senddocu3 628xxx
+┣⊱ Senddocu4 628xxx
+┣⊱ Senddocu5 628xxx
+┗━━⊱
+[ contoh Senddocu 628141052 ]
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+*SEND BUG LOKASI*
+┏━━⊱
+┣⊱ Sendlokas 628xxxx
+┣⊱ Sendlokas2 628xxx
+┣⊱ Sendlokas3 628xxx
+┣⊱ Sendlokas4 628xxx
+┣⊱ Sendlokas5 628xxx
+┗━━⊱
+[ contoh Sendlokas 62814105 ]
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+*SEND BUG INVITE*
+┏━━⊱
+┣⊱ Sendinvite 628xxxx
+┣⊱ Sendinvite2 628xxx
+┣⊱ Sendinvite3 628xxx
+┣⊱ Sendinvite4 628xxx
+┣⊱ Sendinvite5 628xxx
+┗━━⊱
+[ contoh Sendinvite 62814105 ]
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+*SEND BUG LIST*
+┏━━⊱
+┣⊱ Sendbuglist 628xxxx
+┣⊱ Sendbuglist2 628xxx
+┣⊱ Sendbuglist3 628xxx
+┣⊱ Sendbuglist4 628xxx
+┣⊱ Sendbuglist5 628xxx
+┗━━⊱
+[ contoh Sendbuglist 6281410 ]
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+*SEND VIRUS IP / ANDRO*
+┏━━⊱
+┣⊱ Sendvirus 628xxxx
+┣⊱ Sendvirus2 628xxx
+┣⊱ Sendvirus3 628xxx
+┣⊱ Sendvirus4 628xxx
+┣⊱ Sendvirus5 628xxx
+┣⊱ Sendvirus6 628xxx
+┣⊱ Sendvirus7 628xxx
+┣⊱ Sendvirus8 628xxx
+┣⊱ Sendvirus9 628xxx
+┣⊱ Sendvirus10 628xx
+┗━━⊱
+[ contoh Sendvirus 62814105 ]
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 *JADI BUG*
 ┏━━⊱
-┣❏Jadikatalog [reply sticker]
-┣❏Jadijago [ Text nya ]
-┣❏Jadipolling [ Text nya ]
-┣❏Jaditroli [ Text nya ]
-┣❏Jadilokas [ Text nya ]
-┣❏Jadidarknes [ Text nya ]
-┣❏Jadidocu [ Text nya ]
-┣❏Jadibuginvite [ Text nya ]
-┣❏Jadibugpayment [ Text nya ]
-┣❏Jadibugsw [ Text nya ]
-┣❏Jadibugbutton [ Text nya ]
-┣❏Jadivirtext1 [ Text nya ]
-┣❏Jadivirtext2 [ Text nya ]
-┣❏Jadivirtext3 [ Text nya ]
-┣❏Jadivirtext4 [ Text nya ]
-┣❏Jadivirtext5 [ Text nya ]
-┣❏Jadivirtext6 [ Text nya ]
-┣❏Jadivirtext7 [ Text nya ]
-┣❏Jadivirtext8 [ Text nya ]
-┣❏Jadivirtext9 [ Text nya ]
-┣❏Jadivirtext10 [ Text nya ]
+┣⊱ Jadikatalog [reply sticker]
+┣⊱ Jadijago [ Text nya ]
+┣⊱ Jadipolling [ Text nya ]
+┣⊱ Jaditroli [ Text nya ]
+┣⊱ Jadilokas [ Text nya ]
+┣⊱ Jadidarknes [ Text nya ]
+┣⊱ Jadidocu [ Text nya ]
+┣⊱ Jadibuginvite [ Text nya ]
+┣⊱ Jadibugpayment [ Text nya ]
+┣⊱ Jadibugsw [ Text nya ]
+┣⊱ Jadibugbutton [ Text nya ]
+┣⊱ Jadivirtext1 [ Text nya ]
+┣⊱ Jadivirtext2 [ Text nya ]
+┣⊱ Jadivirtext3 [ Text nya ]
+┣⊱ Jadivirtext4 [ Text nya ]
+┣⊱ Jadivirtext5 [ Text nya ]
+┣⊱ Jadivirtext6 [ Text nya ]
+┣⊱ Jadivirtext7 [ Text nya ]
+┣⊱ Jadivirtext8 [ Text nya ]
+┣⊱ Jadivirtext9 [ Text nya ]
+┣⊱ Jadivirtext10 [ Text nya ]
 ┗━━⊱
 [ Contoh Jadijago Casper ]
-▬▭▬▭▬▭▬▭▬▬▭▬▭▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 *BONUS*
 ┏━━⊱
-┣❏Jagoan [ reply chat target ]
-┣❏Sange [ Harus Menjadi Admin ]
+┣⊱ Jagoan [ reply chat target ]
+┣⊱ Sange [ Harus Menjadi Admin ]
 ┗━━⊱`
 exports.bugmenu = bugmenu
